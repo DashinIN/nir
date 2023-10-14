01e392777de47bce931ebd4d35c203c94d3567a8
DashinIN <green19grant@yandex.ru>
2023-10-14 12:42:58 +0300

    framer motion use

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import  { useState } from 'react';
+import { Reorder } from "framer-motion"
 import { utils as XLSXUtils, writeFile as XLSXWriteFile } from 'xlsx';
 import './index.scss'
 
@@ -27,13 +28,52 @@ function App() {
       setLoading(false);
     }
   };
+  const [items, setItems] = useState([])
+  const fields = ['Первый элемент', 'Второй элемент', 'Третий элемент', 'Четвертый элемент', 'Пятый элемент'];
+
+  const handleCheckboxChange = (field) => {
+    if (items.includes(field)) {
+      setItems(items.filter(item => item !== field));
+    } else {
+      setItems([...items, field]);
+    }
+  };
 
   return (
     <div className="App">
-      <h1>Сгенерировать отчет</h1>
+      {/* <h1>Сгенерировать отчет</h1>
       <button onClick={fetchData} disabled={loading}>
         {loading ? 'Загрузка...' : 'Получить таблицу'}
-      </button>
+      </button> */}
+
+      <h2>Отметьте требуемые поля</h2>
+      {fields.map((field, index) => (
+              <div key={index}>
+                <input
+                  type="checkbox"
+                  name="check"
+                  checked={items.includes(field)}
+                  onChange={() => handleCheckboxChange(field)}
+                />
+                <label htmlFor="check">{field}</label>
+              </div>
+            ))}
+
+            { Boolean(items.length) && (
+              <>
+              <h2>Выберете порядок полей</h2>
+                <Reorder.Group values={items} onReorder={setItems} className='itemsWrapper'>
+                  {items.map(item => (
+                    <Reorder.Item key={item} value={item} className='item'>
+                      {item}
+                    </Reorder.Item>
+                  ))}
+                </Reorder.Group>
+               <div>Порядок полей: {items.join(', ').toLowerCase()}</div>
+              </>
+            )    
+        }
+  
     </div>
   );
 }
