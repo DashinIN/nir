fe8f6c8c64960bdeebf87bafdda82a2b5a04f1a5
DashinIN <green19grant@yandex.ru>
2023-10-16 09:25:57 +0300

    framer change version and add linting

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -12,9 +12,28 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    indent: [2, 4],
+    'linebreak-style': 'off',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'react/jsx-indent': [2, 4],
+    'react/jsx-indent-props': [2, 4],
+    'react/jsx-filename-extension': [
+        2,
+        {
+            extensions: ['.js', '.jsx', '.tsx'],
+        },
     ],
-  },
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'off',
+    'react/require-default-props': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': 'off',
+    'no-shadow': 'off',
+    'import/extensions': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'prettier/prettier': 'off',
+},
+
 }
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,81 +1,81 @@
 /* eslint-disable no-unused-vars */
 import  { useState } from 'react';
-import { Reorder } from "framer-motion"
+import { Reorder } from 'framer-motion';
 import { utils as XLSXUtils, writeFile as XLSXWriteFile } from 'xlsx';
-import './index.scss'
+import './index.scss';
 
 function App() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-  const fetchData = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch('http://localhost:5000/getAllTitles');
-      if (!response.ok) {
-        throw new Error('Ошибка при получении данных');
-      }
-      const jsonData = await response.json();
-      setData(jsonData);
-      console.log(jsonData)
-      // const ws = XLSXUtils.json_to_sheet(jsonData);
-      // const wb = XLSXUtils.book_new();
-      // XLSXUtils.book_append_sheet(wb, ws, 'Лист 1');
-      // XLSXWriteFile(wb, 'Таблица.xlsx');
-    } catch (error) {
-      console.error('Ошибка:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
-  const [items, setItems] = useState([])
-  const fields = ['Первый элемент', 'Второй элемент', 'Третий элемент', 'Четвертый элемент', 'Пятый элемент'];
+    const fetchData = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch('http://localhost:5000/getAllTitles');
+            if (!response.ok) {
+                throw new Error('Ошибка при получении данных');
+            }
+            const jsonData = await response.json();
+            setData(jsonData);
+            console.log(jsonData);
+            // const ws = XLSXUtils.json_to_sheet(jsonData);
+            // const wb = XLSXUtils.book_new();
+            // XLSXUtils.book_append_sheet(wb, ws, 'Лист 1');
+            // XLSXWriteFile(wb, 'Таблица.xlsx');
+        } catch (error) {
+            console.error('Ошибка:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+    const [items, setItems] = useState([]);
+    const fields = ['Первый элемент', 'Второй элемент', 'Третий элемент', 'Четвертый элемент', 'Пятый элемент'];
 
-  const handleCheckboxChange = (field) => {
-    if (items.includes(field)) {
-      setItems(items.filter(item => item !== field));
-    } else {
-      setItems([...items, field]);
-    }
-  };
+    const handleCheckboxChange = (field) => {
+        if (items.includes(field)) {
+            setItems(items.filter(item => item !== field));
+        } else {
+            setItems([...items, field]);
+        }
+    };
 
-  return (
-    <div className="App">
-      {/* <h1>Сгенерировать отчет</h1>
+    return (
+        <div className="App">
+            {/* <h1>Сгенерировать отчет</h1>
       <button onClick={fetchData} disabled={loading}>
         {loading ? 'Загрузка...' : 'Получить таблицу'}
       </button> */}
 
-      <h2>Отметьте требуемые поля</h2>
-      {fields.map((field, index) => (
-              <div key={index}>
-                <input
-                  type="checkbox"
-                  name="check"
-                  checked={items.includes(field)}
-                  onChange={() => handleCheckboxChange(field)}
-                />
-                <label htmlFor="check">{field}</label>
-              </div>
+            <h2>Отметьте требуемые поля</h2>
+            {fields.map((field, index) => (
+                <div key={index}>
+                    <input
+                        type="checkbox"
+                        name="check"
+                        checked={items.includes(field)}
+                        onChange={() => handleCheckboxChange(field)}
+                    />
+                    <label htmlFor="check">{field}</label>
+                </div>
             ))}
 
             { Boolean(items.length) && (
-              <>
-              <h2>Выберете порядок полей</h2>
-                <Reorder.Group values={items} onReorder={setItems} className='itemsWrapper'>
-                  {items.map(item => (
-                    <Reorder.Item key={item} value={item} className='item'>
-                      {item}
-                    </Reorder.Item>
-                  ))}
-                </Reorder.Group>
-               <div>Порядок полей: {items.join(', ').toLowerCase()}</div>
-              </>
+                <>
+                    <h2>Выберете порядок полей</h2>
+                    <Reorder.Group values={items} onReorder={setItems} className='itemsWrapper'>
+                        {items.map(item => (
+                            <Reorder.Item key={item} value={item} className='item'>
+                                {item}
+                            </Reorder.Item>
+                        ))}
+                    </Reorder.Group>
+                    <div>Порядок полей: {items.join(', ').toLowerCase()}</div>
+                </>
             )    
-        }
+            }
   
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
