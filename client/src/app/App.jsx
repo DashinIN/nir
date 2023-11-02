512c14c6fcfbbe6e40ffbf3ef3a0514f3bea0511
DashinIN <green19grant@yandex.ru>
2023-11-02 15:36:02 +0300

    начальная конфигурация роутинга

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,21 +1,42 @@
 /* eslint-disable no-unused-vars */
 import  { useState } from 'react';
+import { Route, Routes, NavLink } from 'react-router-dom';
 import './styles/index.scss';
 import CreateTemplate from '@/pages/CreateTemplate/CreateTemplate';
 import UseTemplate from '@/pages/UseTemplate/UseTemplate';
+
 
 function App() {
     const [items, setItems] = useState([]);
 
     return (
         <div className="App">
-            <CreateTemplate 
-                items={items}
-                setItems={setItems}
-            />
-            <UseTemplate
-                items={items}
-            />
+            <NavLink
+                to="/CreateTemplate"
+                className={({ isActive, isPending }) =>
+                    isPending ? 'pending' : isActive ? 'active' : ''
+                }
+            >
+                CreateTemplate
+            </NavLink>
+            <NavLink
+                to="/UseTemplate"
+                className={({ isActive, isPending }) =>
+                    isPending ? 'pending' : isActive ? 'active' : ''
+                }
+            >
+                UseTemplate
+            </NavLink>
+            <Routes>
+                <Route
+                    path={'/CreateTemplate'}
+                    element={<CreateTemplate items={items} setItems={setItems}/>}
+                />
+                <Route
+                    path={'/UseTemplate'}
+                    element={<UseTemplate items={items}/>}
+                />
+            </Routes>
         </div>
     );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render( 
     <QueryClientProvider client={queryClient}>
-        <App />
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
     </QueryClientProvider>
 );
