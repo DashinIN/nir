5d470de28661b951165a8827f42b3d086f36b940
DashinIN <green19grant@yandex.ru>
2023-12-03 08:53:18 +0300

    добавление названий полей на русском в таблицу и изменение логики под это

--- server/index.js
+++ server/index.js
@@ -44,6 +44,7 @@ app.get('/getAllTitles', async (req, res) => {
 
 app.post('/getSelectedSampleData', async (req, res) => {
     const titles = req.body
+    console.log(titles)
     const translatedTitles = translateTitlesEN(titles)
     const requestQuery = selectedFieldsQuery(translatedTitles)
     try {
@@ -60,6 +61,7 @@ app.post('/addSample', async (req, res) => {
         const { name, items } = req.body
 
         console.log(name, items)
+        const titlesRU = items
         const titlesEN = translateTitlesEN(items)
         const insertSampleNameQuery = 'INSERT INTO output_samples (sample_name) VALUES ($1) RETURNING sample_id'
         const sampleName = [name]
@@ -67,8 +69,8 @@ app.post('/addSample', async (req, res) => {
 
         const sampleId = sampleResult.rows[0].sample_id
 
-        const insertSampleFieldsQuery = 'INSERT INTO output_samples_fields (sample_id, field_name, field_order) VALUES ($1, $2, $3)'
-        const sampleFieldsValues = titlesEN.map((item, index) => [sampleId, item, index + 1])
+        const insertSampleFieldsQuery = 'INSERT INTO output_samples_fields (sample_id, field_name_en, field_name_ru, field_order) VALUES ($1, $2, $3, $4)'
+        const sampleFieldsValues = titlesEN.map((item, index) => [sampleId, item, titlesRU[index], index + 1])
 
         await Promise.all(sampleFieldsValues.map(async (values) => {
             await pool.query(insertSampleFieldsQuery, values)
@@ -76,6 +78,7 @@ app.post('/addSample', async (req, res) => {
 
         res.status(201).json({ message: 'Шаблон успешно добавлен' })
     } catch (error) {
+        console.error(error)
         if (error.code === '23505') {
             res.status(400).json({ error: 'Шаблон с таким именем уже существует. Выберете другое название шаблона' })
         } else {
@@ -91,10 +94,11 @@ app.get('/getAllSamples', async (req, res) => {
         // Получаю данные о шаблонах в формате sample_id:, sample_name:
         const result = await Promise.all(samples.map(async (sample) => {
             const sampleId = [sample.sample_id]
-            const fieldsQuery = 'SELECT field_name FROM output_samples_fields WHERE sample_id = $1 ORDER BY field_order'
+            const fieldsQuery = 'SELECT field_name_en FROM output_samples_fields WHERE sample_id = $1 ORDER BY field_order'
             // Получаю поля в порядке их очереди для определенного шаблона
             const { rows: sampleFields } = await pool.query(fieldsQuery, sampleId)
-            const fieldNames = sampleFields.map((field) => field.field_name)
+            console.log(sampleFields)
+            const fieldNames = sampleFields.map((field) => field.field_name_en)
             // Поля добавляю массивом
             return {
                 ...sample,
